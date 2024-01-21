fd7c85679945ecb050727c035c1412524256a9f6
Minh <dovanminh253@gmail.com>
2024-01-21 18:11:58 +0700

    tut 1 va 2

---
+++ tut1/Ex02.java
@@ -0,0 +1,33 @@
+package tut1;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter the range of array: ");
+        int n = inp.nextInt();
+        inp.close();
+        
+        if (n<=0){
+            System.out.println("Error...");
+            return ;
+        }
+
+        System.out.println("Random Arr: " + Arrays.toString(CreatArr(n)));
+    }
+
+    static int [] CreatArr (int n){
+        int [] arr = new int[n];
+        Random random = new Random();
+
+        for (int i=0; i<n; i++){
+            arr[i] = random.nextInt();
+        }
+        
+        return arr;
+    }
+}
