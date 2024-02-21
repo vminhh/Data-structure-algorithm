1a9395b4f556d5dd09b785fc1d7b74cdcf45ab50
Minh <dovanminh253@gmail.com>
2024-02-21 14:32:13 +0700

    done tut 4

---
+++ tut4/HighMarksStu.java
@@ -0,0 +1,56 @@
+package tut4;
+
+import java.util.*;
+
+public class HighMarksStu {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter the number of list: ");
+        int n = inp.nextInt();
+
+        List<Student> students = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            System.out.print("Name: ");
+            String name = inp.next();
+            System.out.print("Mark: ");
+            int mark = inp.nextInt();
+            students.add(new Student(name, mark));
+            System.out.println("------------");
+        }
+
+        System.out.print("Enter M: ");
+        int m = inp.nextInt();
+
+        Collections.sort(students, Collections.reverseOrder());
+
+        System.out.println("Top " + m + " students:");
+        for (int i = 0; i < Math.min(m, students.size()); i++) {
+            System.out.println(students.get(i).getName());
+        }
+
+        inp.close();
+    }
+
+    static class Student implements Comparable<Student> {
+        private String name;
+        private int mark;
+
+        public Student(String name, int mark) {
+            this.name = name;
+            this.mark = mark;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public int getMark() {
+            return this.mark;
+        }
+
+        public int compareTo(Student other) {
+            return Integer.compare(this.mark, other.mark);
+        }
+    }
+}
---
+++ tut4/ReArrangeArr.java
@@ -0,0 +1,39 @@
+package tut4;
+
+import java.util.*;
+
+public class ReArrangeArr {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter the arr length: ");
+        int n = inp.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the values of arr: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = inp.nextInt();
+        }
+
+        System.out.println("Result: " + Arrays.toString(bub(arr)));
+        inp.close();
+    }
+
+    static int[] bub(int[] a) {
+        int n = a.length;
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for (int i = 1; i < n; i++) {
+                if (a[i] < a[i - 1]) {
+                    int temp = a[i - 1];
+                    a[i - 1] = a[i];
+                    a[i] = temp;
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
+        return a;
+    }
+}
